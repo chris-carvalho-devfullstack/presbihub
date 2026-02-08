36344a4521aa484a7deb05bd9cfee571199dec4c
christian <christian.carvalho.adv@gmail.com>
2026-02-08 13:12:03 -0300

    export const runtime = 'edge' adicionado para compatibilidade na buil do projeto na cloudfare

--- app/dashboard/cursos/[id]/page.tsx
+++ app/dashboard/cursos/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+export const runtime = "edge";
+
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
