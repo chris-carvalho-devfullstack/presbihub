21d5c8316c73deed629b8dbc807b2ae663a26bff
christian <christian.carvalho.adv@gmail.com>
2026-02-15 23:42:50 -0300

    Troca da fonte geist que é incompativel com versao 14 next

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// Configuração da fonte Inter (compatível com Next.js 14)
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "IPPC - Igreja Presbiteriana de Poços de Caldas",
@@ -25,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.className} antialiased`}>
         {children}
       </body>
     </html>
